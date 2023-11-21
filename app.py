7b19ef20f270e3dd8702776558269fc483690e8e
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-21 14:39:36 -0800

    one more change

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, render_template
+from .stories import Story
 
 app = Flask(__name__)
+story = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Once upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+)
+
 
 @app.route('/')
 def show_form():
     '''Home page shows the madlibs beginning form'''
-    return render_template("form.html")
+    return render_template("form.html", story)
